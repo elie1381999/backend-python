189c2943071af74cc0b1aa721c8286df23b16d4f
elie1381999 <eliebousleiman14@gmail.com>
2025-08-19 04:11:23 +0300

    Update webhook_handler.py

--- central-bot/webhook_handler.py
+++ central-bot/webhook_handler.py
@@ -1,3 +1,4 @@
+'''
 import os
 import asyncio
 import json
@@ -1009,3 +1010,4 @@ async def handle_callback_query(callback_query: Dict[str, Any]):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+'''
