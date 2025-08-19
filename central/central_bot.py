8e1ded176c7072a5fc816f001fd88c4004edd910
elie1381999 <eliebousleiman14@gmail.com>
2025-08-19 15:38:34 +0300

    Update central_bot.py

--- central/central_bot.py
+++ central/central_bot.py
@@ -5,7 +5,7 @@ import httpx
 from fastapi import FastAPI, Request, Header
 from starlette.responses import PlainTextResponse, Response
 from typing import Optional
-from .utils import BOT_TOKEN, WEBHOOK_URL, ADMIN_CHAT_ID, supabase, logger, send_message, award_points, has_history, now_iso, safe_clear_markup, supabase_find_registered, create_menu_options_keyboard, create_main_menu_keyboard, USER_STATES
+from central.utils import supabase_insert_return, supabase_update_by_id_return, supabase_find_draft, supabase_find_registered, send_message, create_language_keyboard, create_gender_keyboard, create_interests_keyboard, create_main_menu_keyboard, get_state, set_state, award_points, has_history, STARTER_POINTS, POINTS_REFERRAL_JOIN, logger, safe_clear_markup, edit_message_keyboard, uuid, USER_STATES
 from handlers.central_handler import handle_start, handle_menu, handle_language_selection, handle_gender_selection, handle_interests_selection
 from handlers.points_handler import handle_points
 from handlers.profile_handler import handle_profile, handle_phone_contact, handle_dob_input
